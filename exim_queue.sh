114f191ad7166a90e2d226a61b271db35a76c78b
dheny.priatna <dheny.priatna@shopee.com>
2023-05-09 10:44:09 +0700

    add exim queue text file collector

---
+++ exim_queue.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -e
+
+# Author: Dheny @furiatona on github
+# Generate Exim queue number
+# which are not handled by node_exporter's own collector
+
+# Sample output
+# [root@example.local ~]# exim -bpc
+# 55
+
+EXIM_CMD=$(exim -bpc)
+CMD_TIMEOUT="timeout 5"
+
+$CMD_TIMEOUT exim -bpc > /dev/null
+exit_status=$?
+if [[ $exit_status -eq 124 ]]; then
+cat <<EOS
+exim_queue 999999999
+EOS
+else
+cat <<EOS
+exim_queue $EXIM_CMD
+EOS
+fi
